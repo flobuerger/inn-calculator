66559747119b74b77280c2499e5e8adc6ce68666
Florian Bürger <florian.buerger>
2023-03-11 21:43:53 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inn-calculator-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'inn-calculator';
+}
